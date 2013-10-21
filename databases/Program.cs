31add07aa2870b9a14bedac23ab9972abbd63781
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2013-10-21 13:57:41 -0500

    Applied some techniques from class. Last not saved, crash.
    Xamarin crashed after the last build. Or during, depending on how you
    look at it. I was trying to see if it would take two separate results
    and print them out one after the other, should be no problem, but
    instead it crashed/froze.

--- databases/Program.cs
+++ databases/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Data;
+using Mono.Data;
 using Mono.Data.Sqlite;
 
 namespace databases
@@ -12,11 +13,37 @@ namespace databases
 			SqliteConnection db = new SqliteConnection(connectionInfo);
 			db.Open ();
 
-			Console.WriteLine (db.State.ToString ());
+			Console.Write ("Please enter a movieID: ");
+			string input = Console.ReadLine();
+			int movieid = int.Parse(input);
+
+			SqliteCommand cmd = new SqliteCommand(db); 
+			cmd.CommandText = "SELECT MovieName FROM Movies WHERE MovieID = " + movieid + ";" ;
+			object result = cmd.ExecuteScalar();
+
+			Console.Write ("Please enter a movieID: ");
+			string input2 = Console.ReadLine();
+			int movieid2 = int.Parse(input2);
+
+			cmd.CommandText = @"SELECT AVG(Rating) FROM Reviews
+			INNER JOIN Movies ON Reviews.MovieID = Movies.MovieID WHERE Movies.MovieName = " + movieid2 + ";";
+			object result2 = cmd.ExecuteScalar();
+
+			// just to play? 
+			// string sql = "CREATE TABLE [Movies] ([MovieID] INTEGER PRIMARY KEY, [Movie Name]
+			/*
+			*	SqliteCommand cmd = new SqliteCommand(db); 
+			*	cmd.CommandText = "CREATE TABLE [Movies] ([MovieID] INTEGER PRIMARY KEY,[MovieName] TEXT);" ;
+			*	cmd.ExecuteNonQuery();
+			*	cmd.CommandText = @"INSERT INTO Movies(MovieID, MovieName) Values(1, 'When Harry Met Salley');";
+			*/
+
+			Console.WriteLine (result);
+			Console.WriteLine (result2);
 			db.Close ();
 
 
-			Console.WriteLine ("Hello World!");
+
 		}
 	}
 }
