2d1e02fb12d6c8d5bc4b93aa2139660c9aadfdfe
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2013-10-25 12:10:06 -0500

    Changed name to fit it's role.

--- databases/Properties/AssemblyInfo.cs
+++ databases/Properties/AssemblyInfo.cs
@@ -4,13 +4,13 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 [assembly: AssemblyTitle ("databases")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("")]
+[assembly: AssemblyDescription ("pretty, oh so pretty")]
+[assembly: AssemblyConfiguration ("Alpha")]
+[assembly: AssemblyCompany ("The Noventa Foundation")]
+[assembly: AssemblyProduct ("Assignments")]
 [assembly: AssemblyCopyright ("Noventa")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
+[assembly: AssemblyTrademark ("Netflix")]
+[assembly: AssemblyCulture ("Cultured")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
