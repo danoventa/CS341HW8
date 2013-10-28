e92300347ae1e19b50622d4399d363d5303773a4
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2013-10-28 13:00:28 -0500

    Updated the business and data access files.

--- databases/Business.cs
+++ databases/Business.cs
@@ -19,23 +19,73 @@ namespace databases
 
 			DataAccess dat = new DataAccess("Netflix.sqlite");
 
+			public void totalMovies()
+			{
+				Console.WriteLine("Printing out all movies in the database:");
 
+			}
 
+			public void nameLookUp()
+			{
+				Console.WriteLine("Please insert Movie Name");
+				string movie = Console.ReadLine();
 
-			/*	
-			 * 	1. Ability to retrieve total # of movies 
-			 *	2. Ability to lookup a movie name from a movie id 
-			 *	3. Ability to lookup a movie id from a movie name 
-			 *	4. Ability to retrieve information about all the movies: ids, names, number of reviews, average rating, 
-			 *		lowest rating, and highest rating; also the ability to foreach through this data 
-			 *	5. Ability to retrieve top-10 movies with the highest average ratings 
-			 *	6. Ability to retrieve total # of reviews 
-			 *	7. Ability to retrieve average rating across all the reviews 
-			 *	8. Ability to retrieve information about a user based on user id: number of distinct movies reviewed, 
-			 *		number of reviews submitted, and average rating given by user across all their reviews 
-			 *	9. Ability to retrieve top-10 users who have submitted the most reviews 
-			 *	10. Ability to insert a new review into the database
-			 */
+				Console.WriteLine(movie + " has ID of:" /* + id */ );
+
+			}
+
+			public void IDLookUp()
+			{
+				Console.WriteLine("Please insert Movie ID");
+				string inID1 = Console.ReadLine();
+				int id1 = int.Parse(inID1);
+
+				Console.WriteLine("Movie with ID of " + id1 + " is: " /* + movie name */);
+			}
+
+			public void convolutedFunction()
+			{
+				Console.WriteLine("This is where the magic happens");
+				/* magic */
+			}
+
+			public void topTenMovies()
+			{
+				Console.WriteLine("Printing top ten movies ");
+
+			}
+
+			public void totalReviews()
+			{
+				Console.WriteLine("All Reviews");
+			}
+
+			public void averageRatings()
+			{
+				Console.WriteLine("Average ratings accross all reviews");
+			}
+
+			public void userInfo()
+			{
+				Console.WriteLine("Which user would you like to spy on?");
+				string inID2 = Console.ReadLine();
+				int id2 = int.Parse(inID2);
+
+				/* get user info */
+			}
+
+			public void topTenUsers()
+			{
+				Console.WriteLine ("Printing top ten users ");
+			}
+
+			public void addReview()
+			{
+				Console.WriteLine("Please insert your review ");
+				string review = Console.ReadLine();
+			}
+
+			return 0;
 		}
 	}
 }
--- databases/DataAccess.cs
+++ databases/DataAccess.cs
@@ -16,31 +16,7 @@ namespace databases
 		public DataAccess ()
 		{
 
-
-
-			cmd.CommandText = @"SELECT AVG(Rating) FROM Reviews
-			INNER JOIN Movies ON Reviews.MovieID = Movies.MovieID WHERE Movies.MovieName = " + movieid2 + ";";
-			object result2 = cmd.ExecuteScalar();
-
-			s
-			Console.Write ("Please enter a movieID: ");
-			string input = Console.ReadLine();
-			int movieid = int.Parse(input);
-
-			SqliteCommand cmd = new SqliteCommand(db); 
-			cmd.CommandText = "SELECT MovieName FROM Movies WHERE MovieID = " + movieid + ";" ;
-			object result = cmd.ExecuteScalar();
-			Console.WriteLine (result);
-
-
-
-			String sql = "SELECT userID, AVG(Rating) FROM Reviews GROUP BY UserID;"
-				DataSet AppDomainSetup = DAL.findObjects(sql);
-			DataTable DateTime = ds.Tables["Table"];
-
-			foreach(EnvironmentVariableTarget ICollection int dt.Columns){
-				Console.WriteLine(ICollection);
-			}
+		
 
 	
 
